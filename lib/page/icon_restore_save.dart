07d0dd41776dd86f5521352a27a94011e783a7b5
luckygogreen <luckygogreen@users.noreply.github.com>
2021-11-15 09:24:18 -0500

    icon

--- lib/page/icon_restore_save.dart
+++ lib/page/icon_restore_save.dart
@@ -1,6 +1,8 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:line_icons/line_icon.dart';
+import 'package:line_icons/line_icons.dart';
 
 class IconSaveDatabaseOrRestore extends StatefulWidget {
   IconSaveDatabaseOrRestore({Key? key}) : super(key: key);
@@ -14,13 +16,13 @@ class _IconSaveDatabaseOrRestoreState extends State<IconSaveDatabaseOrRestore> {
   @override
   Widget build(BuildContext context) {
     //////////// 获取图标的INT数值 用于存数据库 ////////////
-    int iconInt = Icons.access_alarm.codePoint.toInt(); // 获取Icon的数字 57400
-    Map IconToDatabse = {'icon': iconInt, 'fontfamily': 'MaterialIcons'};
-    String jsonIcon = jsonEncode(IconToDatabse); // 最终存入数据库的值 jsonIcon
+    int iconInt1 = Icons.access_alarm.codePoint.toInt(); // 获取Icon的数字 57400
+    Map IconToDatabse1 = {'icon': iconInt1, 'fontfamily': 'MaterialIcons'};
+    String jsonIcon1 = jsonEncode(IconToDatabse1); // 最终存入数据库的值 jsonIcon
 
     //////////// 从数据库获取ICON并打印出来 ////////////
-    Map iconObj =
-        jsonDecode(jsonIcon); // {icon: 57400, fontfamily: MaterialIcons}
+    // 例如 {icon: 57400, fontfamily: MaterialIcons}
+    Map iconObj1 = jsonDecode(jsonIcon1);
 
     // 获取颜色的值 可以存入数据库
     int colorValue = Colors.indigo.value; // int  4282339765
@@ -31,10 +33,16 @@ class _IconSaveDatabaseOrRestoreState extends State<IconSaveDatabaseOrRestore> {
       ),
       body: Container(
         child: Center(
-          child: Icon(
-            //////////// 显示从数据库获取的 ////////////
-            IconData(iconObj['icon'], fontFamily: iconObj['fontfamily']),
-            color: Color(colorValue),
+          child: Column(
+            children: [
+              Icon(
+                //////////// 显示从数据库获取的 ////////////
+                IconData(iconObj1['icon'], fontFamily: iconObj1['fontfamily']),
+                color: Color(colorValue),
+              ),
+              //////////// 第三方图标库里的图标  ////////////
+              Icon(LineIcons.smilingFace),
+            ],
           ),
         ),
       ),
